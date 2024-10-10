89aba325b394da4d091d3ac6c20709ab5e9e34f6
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-10-10 14:28:21 +0200

    feat(ui): add Tailwind CSS

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="flex justify-center items-center h-screen">
+    <p class="text-4xl font-bold text-green-500">Tailwind CSS</p>
   </div>
 </template>
