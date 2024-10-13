59d78516dd1201bf0ba0bcbb1222d27ef47d093f
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-10-13 12:58:25 +0200

    refactor(ui): enhance Badge component styling

--- components/ui/badge/Badge.vue
+++ components/ui/badge/Badge.vue
@@ -1,16 +1,24 @@
 <script setup lang="ts">
-import type { HTMLAttributes } from 'vue'
-import { type BadgeVariants, badgeVariants } from '.'
-import { cn } from '@/lib/utils'
+import type { HTMLAttributes } from "vue";
+import { type BadgeVariants, badgeVariants } from ".";
+import { cn } from "@/lib/utils";
 
 const props = defineProps<{
-  variant?: BadgeVariants['variant']
-  class?: HTMLAttributes['class']
-}>()
+  variant?: BadgeVariants["variant"];
+  class?: HTMLAttributes["class"];
+}>();
 </script>
 
 <template>
-  <div :class="cn(badgeVariants({ variant }), props.class)">
+  <div
+    :class="
+      cn(
+        'flex items-center justify-center gap-2',
+        badgeVariants({ variant: props.variant || 'outline' }),
+        props.class
+      )
+    "
+  >
     <slot />
   </div>
 </template>
