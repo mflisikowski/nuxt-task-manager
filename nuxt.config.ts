fc40f7ea230a953885d65c4680d496a98b9c9956
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-10-14 19:36:01 +0200

    chore(config): re-enable TypeScript strict mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@pinia/nuxt"],
-  // nitro: {
-  //   preset: "vercel",
-  // },
-  // typescript: {
-  //   strict: true,
-  // },
+  typescript: {
+    strict: true,
+  },
   tailwindcss: {
     cssPath: "~/assets/css/tailwind.css",
     configPath: "tailwind.config.ts",
