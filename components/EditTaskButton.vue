46676aabd8f916f127e3f8b1cb87090c152c684d
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-10-14 19:30:59 +0200

    feat(ui): add EditTaskButton component

---
+++ components/EditTaskButton.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { Task } from "@/server/schema";
+import { PencilIcon } from "lucide-vue-next";
+import TaskDialog from "@/components/TaskDialog.vue";
+
+defineProps<{
+  task: Task;
+}>();
+</script>
+
+<template>
+  <TaskDialog
+    :task="task"
+    :triggerButton="{
+      icon: PencilIcon,
+      variant: 'ghost',
+      size: 'icon',
+    }"
+    title="Edit Task"
+  />
+</template>
