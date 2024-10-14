075717bb1c97fa445b677d8e691db06869c8bd56
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-10-14 19:29:57 +0200

    refactor(ui): simplify DataTableActions component

--- components/table/DataTableActions.vue
+++ components/table/DataTableActions.vue
@@ -1,31 +1,30 @@
 <script setup lang="ts">
-import { MoreHorizontal } from "lucide-vue-next";
-import { Button } from "@/components/ui/button";
+import type { Task } from "@/server/schema";
 
-import {
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-  DropdownMenuItem,
-  DropdownMenu,
-} from "@/components/ui/dropdown-menu";
+import { useTaskStore } from "@/stores/useTaskStore";
+import EditTaskButton from "@/components/EditTaskButton.vue";
+import { Button } from "@/components/ui/button";
+import { TrashIcon } from "lucide-vue-next";
+
+defineProps<{
+  task: Task;
+}>();
+
+const taskStore = useTaskStore();
+
+const deleteTask = async (id: string) => {
+  if (confirm("Are you sure you want to delete this task?")) {
+    await taskStore.deleteTask(id);
+  }
+};
 </script>
 
 <template>
-  <DropdownMenu>
-    <DropdownMenuTrigger as-child>
-      <Button aria-haspopup="true" variant="ghost" size="icon">
-        <MoreHorizontal class="h-4 w-4" />
-        <span class="sr-only">Toggle menu</span>
-      </Button>
-    </DropdownMenuTrigger>
+  <div class="flex gap-2 items-center">
+    <EditTaskButton :task="task" />
 
-    <DropdownMenuContent class="bg-white">
-      <DropdownMenuItem>
-        <Button variant="ghost">Edit</Button>
-      </DropdownMenuItem>
-      <DropdownMenuItem>
-        <Button variant="ghost">Delete</Button>
-      </DropdownMenuItem>
-    </DropdownMenuContent>
-  </DropdownMenu>
+    <Button variant="ghost" @click="deleteTask(task.id)">
+      <TrashIcon class="h-4 w-4 text-red-500" />
+    </Button>
+  </div>
 </template>
