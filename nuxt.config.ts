30d2f7be320785dbd7d8fd0a8190a36577d12145
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-10-14 18:16:27 +0200

    chore(config): adjust Nuxt configuration for Vercel deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@pinia/nuxt"],
-  nitro: {
-    preset: "vercel",
-  },
-  typescript: {
-    strict: true,
-  },
+  // nitro: {
+  //   preset: "vercel",
+  // },
+  // typescript: {
+  //   strict: true,
+  // },
   tailwindcss: {
     cssPath: "~/assets/css/tailwind.css",
     configPath: "tailwind.config.ts",
